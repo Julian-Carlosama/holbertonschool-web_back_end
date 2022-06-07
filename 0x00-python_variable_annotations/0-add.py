9623d9b82df2e0568bb13b4369653378e3aa1427
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-06-06 21:42:18 -0500

    Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Module that add that takes a float a and a float b and returns their sum as a float. """
+
+
+def add(a: float, b: float) -> float:
+    """ Return the sum between a and b. """
+    return a + b
