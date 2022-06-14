e17ad5c5b3b8946ff26ce2c46a60577f05e403d3
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-06-14 09:44:11 -0500

    Simple pagination

---
+++ 0x04-pagination/1-simple_pagination.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+""" Simple pagination """
+import csv
+import math
+from typing import List, Tuple
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """ Function that return the index range"""
+
+    start = (page-1) * page_size
+    end = page * page_size
+    range = (start, end)
+    return range
+
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        assert type(page) is int and page > 0
+        assert type(page_size) is int and page_size > 0
+        gp_range = index_range(page, page_size)
+        gp_start = gp_range[0]
+        gp_end = gp_range[1]
+        return self.dataset()[gp_start:gp_end]
