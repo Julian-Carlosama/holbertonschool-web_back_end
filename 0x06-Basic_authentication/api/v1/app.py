c3219b6d99445f4337c031c09444f9c95539cbb3
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-06-21 19:29:36 -0500

    Add new task

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -13,7 +13,24 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+if os.getenv("AUTH_TYPE") == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+else:
+    from api.v1.auth import Auth
+    auth = Auth()
 
+@app.before_request
+def beforeRequest():
+    if auth is None:
+        return
+    paths = ['/api/v1/status/', '/api/v1/unauthorized', '/api/v1/forbiden/']
+    if not auth.require_auth(request.path, paths):
+        return
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
 
 @app.errorhandler(404)
 def not_found(error) -> str:
@@ -24,7 +41,6 @@ def not_found(error) -> str:
 @app.errorhandler(401)
 def unauthorized(error) -> str:
     return jsonify({"error": "Unauthorized"}), 401
-
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
