e6ccb9f03edb290512db2b1474dad673c1121031
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-07-01 23:29:05 -0500

    Add task auth

---
+++ 0x08-user_authentication_service/auth.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" Module AUTH """
+
+from db import DB
+from user import User
+from bcrypt import hashpw, gensalt, checkpw
+from sqlalchemy.orm.exc import NoResultFound
+from uuid import uuid4
+from typing import Union
+
+
+def _generate_uuid() -> str:
+    """
+       Returns a string representation
+       of a new UUID.
+    """
+    return str(uuid.uuid1())
