bdb7bb7e8b9b970001a45aae1e98ff15606f5f48
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-07-01 23:43:04 -0500

    Add task 5

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -9,6 +9,27 @@ from uuid import uuid4
 from typing import Union
 
 
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Creates and saves a new user given an email and a password
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError("User {} already exists.".format(email))
+        except ValueError:
+            raise
+        except Exception:
+            pwd = _hash_password(password)
+            user = self._db.add_user(email, pwd)
+            return user
+
+
 def _hash_password(password: str) -> str:
     """ Returns a hashed password """
     return hashpw(password.encode("utf-8"), gensalt())
