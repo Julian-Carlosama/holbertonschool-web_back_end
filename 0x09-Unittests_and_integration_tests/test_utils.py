6644eb88f4a9238df34583151153cd979d9ae5a2
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-07-06 00:15:43 -0500

    First implements fors this task

---
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Module Unnitest """
+
+from importlib.resources import path
+from unittest import TestCase
+
+class TestAccessNestedMap(TestCase):
+    """ Testing function Map """
+    @parameterized.expand()
+    
+    def TestAccessNestedMap.test_access_nested_map(self, path):
+        """ Methos that process"""
+        
