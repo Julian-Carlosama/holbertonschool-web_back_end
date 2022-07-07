20cec88d954e026eaa85ba9865882303dfc18171
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-07-06 22:33:47 -0500

    Add task 0 and 1

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
 """ Module Unnitest """
 
-from importlib.resources import path
+import unittest
+from unittest.mock import patch
+from parametrized import parameterized
 from unittest import TestCase
+from utils import access_nested_map
+
 
 class TestAccessNestedMap(TestCase):
     """ Testing function Map """
-    @parameterized.expand()
-    
-    def TestAccessNestedMap.test_access_nested_map(self, path):
-        """ Methos that process"""
-        
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {'b': 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, mapp, patch, outputResult):
+        """ Testing for access_nested-map """
+        self.assertEqual(access_nested_map(map, path), outputResult)
+
+    @parameterized.expand([
+        ({}, ("a",), 'a'),
+        ({"a": 1}, ("a", "b"), 'b')
+    ])
+    def test_access_nested_map_exception(self, map, outError):
+        """ Method that access nested exection """
+        with self.assertRaises(keyError) as e:
+            access_nested_map(map, path)
+            self.assertEqual(outError, e.exception)
