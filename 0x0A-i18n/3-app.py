cbe591979f1e09e806870ee5b72e02776a637980
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-07-07 23:24:46 -0500

    Update task 2 and 3

--- 0x0A-i18n/3-app.py
+++ 0x0A-i18n/3-app.py
@@ -2,10 +2,9 @@
 """ Create routes app """
 
 
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
 from flask_babel import Babel
 from typing import List
-
 app = Flask(__name__)
 babel = Babel(app)
 
