e7f4d21f3083a42d6cb1dd2a5067f3d8ccb7c3f3
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-07-08 00:03:07 -0500

    add task

---
+++ 0x0A-i18n/4-app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+""" Create routes app """
+
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+from typing import List
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """ Class config """
+    LANGUAGES = ["en", "fr"]
+
+
+app.config.from_object(Config)
+Babel.default_locale = "en"
+Babel.default_timezone = "UTC"
+
+
+@babel.localeselector
+def get_locale():
+    """ Function that determine
+    the best match with our supported languages.
+    """
+    locale = request.args.get("locale")
+    if locale:
+           return locale
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+@app.route("/", methods=["GET"], strict_slashes=False)
+def index():
+    """ get the route"""
+    return render_template("4-index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
