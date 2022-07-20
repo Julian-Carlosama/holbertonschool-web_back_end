178b13dc64f58d2418825602f9fa208eab723277
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-07-19 21:54:54 -0500

    Update new task

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -7,6 +7,35 @@ from sys import byteorder
 from functools import wraps
 
 
+def call_history(method: Callable) -> Callable:
+    """ Store the history of inputs and outputs for a particular function """
+    key = method.__qualname__
+    inputs = key + ":inputs"
+    outputs = key + ":outputs"
+
+    @wraps(method)
+    def wrapper(self, *args, **kwds):
+        """ Wrapped method """
+        self._redis.rpush(inputs, str(args))
+        data = method(self, *args, **kwds)
+        self._redis.rpush(outputs, str(data))
+        return data
+    return wrapper
+
+
+def count_calls(method: Callable) -> Callable:
+    """ to count how many times methods of the Cache class are called """
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwds):
+        """ wrapped function """
+        self._redis.incr(key)
+        return method(self, *args, **kwds)
+    return wrapper
+
+
+
 class Cache:
     """ Class private instance of the Redis client """
     def __init__(self):
