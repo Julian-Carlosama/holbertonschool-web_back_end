6dfd7a58eb7c3d74a00be881e9926b4476532ee7
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-07-21 22:35:22 -0500

    Add new project

---
+++ 0x0C-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- Create a table called users if it doesn't exist
+-- Table with  country item
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM ('US', 'CO', 'TN') NOT NULL
+);
