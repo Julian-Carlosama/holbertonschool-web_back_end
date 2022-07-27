c588c9280b16e2d453377e28a7bfe4158aa78563
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-07-26 23:37:30 -0500

    Add task 8

---
+++ 0x0D-NoSQL/8-all.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Lists all documents in a collection
+"""
+
+
+def list_all(mongo_collection):
+	"""
+    Function that lists all documents in a collection
+    """
+
+    documents = mongo_collection.find()
+    if documents.count() > 0:
+        return documents
+    return []
