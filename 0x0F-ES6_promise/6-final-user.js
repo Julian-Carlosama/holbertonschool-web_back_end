65f8617bbe01de0b75c1fb01be1f83a506f05f10
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-08-03 00:21:15 -0500

    Update task 6

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -1,5 +1,5 @@
-import signUpUser from './4-all-reject';
-import uploadPhoto from './5-all-reject';
+import signUpUser from './4-user-promiser';
+import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   const user = await signUpUser(firstName, lastName);
