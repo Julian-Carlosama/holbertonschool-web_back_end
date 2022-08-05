c39fce2a7b037409e5d6a13190847326d2ed2f35
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-08-04 21:58:11 -0500

    Fix task 2

--- 0x10-ES6_classes/2-hbtn_course.js
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -16,19 +16,19 @@ export default class HolbertonCourse {
 
   // Setters
   set name(new_name) {
-    if (Object.getPrototypeOf(new_name) !== String.prototype) throw TypeError('name must be a string');
+    if (Object.getPrototypeOf(new_name) !== String.prototype) throw TypeError('Name must be a string');
     this._name = new_name;
   }
 
   set length(new_length) {
-    if (Object.getPrototypeOf(new_length) !== Number.prototype) throw TypeError('length must be a number');
+    if (Object.getPrototypeOf(new_length) !== Number.prototype) throw TypeError('Length must be a number');
     this._length = new_length;
   }
 
   set students(new_students) {
-    if (Object.getPrototypeOf(new_students) !== Array.prototype) throw TypeError('students must be an array');
+    if (Object.getPrototypeOf(new_students) !== Array.prototype) throw TypeError('Students must be an array');
     new_students.forEach((student) => {
-      if (Object.getPrototypeOf(student) !== String.prototype) throw TypeError('students must be an array of strings');
+      if (Object.getPrototypeOf(student) !== String.prototype) throw TypeError('Students must be an array of strings');
     });
     this._students = new_students;
   }
