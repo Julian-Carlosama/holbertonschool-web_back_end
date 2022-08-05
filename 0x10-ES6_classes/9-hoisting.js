3c8f71a93382dbf3b809bc1257f425cd01fe5a63
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-08-04 23:10:44 -0500

    Update task 9

--- 0x10-ES6_classes/9-hoisting.js
+++ 0x10-ES6_classes/9-hoisting.js
@@ -12,12 +12,11 @@ export class HolbertonClass {
     return this._location;
   }
 }
-
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
 export class StudentHolberton {
-  constructor(firstName, lastName) {
+  constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
     this._holbertonClass = holbertonClass;
@@ -31,9 +30,6 @@ export class StudentHolberton {
     return this.holbertonClass;
   }
 
-  get fullStudentDescription() {
-    return `${self._firstName} ${self._lastName} - ${self._holbertonClass.year} - ${self._holbertonClass.location}`;
-  }
 }
 
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
