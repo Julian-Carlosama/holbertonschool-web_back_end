d7e258e0d8c6e0b2cb826e02b24bf26b02de8c5f
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-08-04 20:07:03 -0500

    Let's make some classrooms

---
+++ 0x10-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,11 @@
+import ClassRoom from "./0-classroom";
+
+export default function initializeRooms() {
+  const sizee = [19, 20, 34];
+  const array = [];
+
+  for (const size of sizee) {
+    array.push(new ClassRoom(size));
+  }
+  return array;
+}
