7f9264b7bad86cf1e2635fe11d6f9de89298f949
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-08-18 23:48:40 -0500

    update task

--- 0x13-unittests_in_js/1-calcul.js
+++ 0x13-unittests_in_js/1-calcul.js
@@ -1,17 +1,19 @@
 module.exports = function calculateNumber(type, a, b) {
+  const A = Number(a);
+  const B = Number(b);
+
+  if (Number.isNaN(A) || Number.isNaN(B))
+  throw TypeError;
+
   if (type === 'SUM') {
-    return (Math.round(a) + Math.round(b));
-  }
-  else if (type === 'SUBTRACT') {
-    return (Math.round(a) - Math.round(b));
-    
-  }
-  else if (type === 'DIVIDE') {
-    if (Math.round(b) === 0) {
+    return (Math.round(A) + Math.round(B));
+  } else if (type === 'SUBTRACT') {
+    return (Math.round(A) - Math.round(B));
+  } else if (type === 'DIVIDE') {
+    if (Math.round(B) === 0) {
       return ('Error');
-    }
-    else {
-      return (Math.round(a) / Math.round(b));
+    } else {
+      return (Math.round(A) / Math.round(B));
     }
   }
 }
