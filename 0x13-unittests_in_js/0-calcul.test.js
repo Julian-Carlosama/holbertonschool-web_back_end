52a3d3c33eee869772b723991900d698303af2c0
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-08-20 22:58:47 -0500

    Update

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -35,4 +35,12 @@ describe('calculateNumber', () => {
   it('Testing throw error', () => {
     assert.throws(() => calculateNumber(NaN, 5), 'The argument not is a number');
   });
+  it('it should return the right number', () => {
+    assert.equal(calculateNumber(1.3, 0), 1);
+    assert.equal(calculateNumber(0, 1.2), 1);
+    assert.equal(calculateNumber(1.3, 1.3), 2);
+    assert.equal(calculateNumber(1.7, 1.2), 3);
+    assert.equal(calculateNumber(1.3, 1.8), 3);
+    assert.equal(calculateNumber(1.6, 1.8), 4);
+  });
 });
