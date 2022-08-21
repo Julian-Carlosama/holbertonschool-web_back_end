25ee1d79ce1cb5e4f1f86a69e4b86a10440336b5
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-08-20 22:53:22 -0500

    Update task

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -4,6 +4,8 @@ const calculateNumber = require("./0-calcul.js");
 describe('calculateNumber', () => {
   it('Round the result of a + b numbers', () => {
     assert.equal(calculateNumber(3.9, 5.6), 10);
+    assert.equal(calculateNumber(1.7, 0), 2);
+    assert.equal(calculateNumber(1.3, 0), 1);
   });
   it('Show the result of a + b negative numbers', () => {
     assert.equal(calculateNumber(-7, 3), -4);
@@ -26,6 +28,9 @@ describe('calculateNumber', () => {
   it('Tetsting for two integers numbers', () => {
     assert.equal(calculateNumber(0, 0), 0);
     assert.equal(calculateNumber(0.0, 0), 0);
+    assert.equal(calculateNumber(0, 1.0), 1);
+    assert.equal(calculateNumber(0, 1.3), 1);
+    assert.equal(calculateNumber(0, 1.7), 2);
   });
   it('Testing throw error', () => {
     assert.throws(() => calculateNumber(NaN, 5), 'The argument not is a number');
