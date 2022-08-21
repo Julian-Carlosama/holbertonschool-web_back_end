14cbc5de01c051e4f33fc88a630ddb75fa3a2ec9
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-08-20 22:44:54 -0500

    Update task 0

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -1,33 +1,33 @@
 const assert = require('assert');
 const calculateNumber = require("./0-calcul.js");
 
-describe('calculateNumber', function() {
-  it('Round the result of a + b numbers', function() {
+describe('calculateNumber', () => {
+  it('Round the result of a + b numbers', () => {
     assert.equal(calculateNumber(3.9, 5.6), 10);
   });
-  it('Show the result of a + b negative numbers', function() {
+  it('Show the result of a + b negative numbers', () => {
     assert.equal(calculateNumber(-7, 3), -4);
   });
-  it('Show the result of a + b negative numbers', function() {
+  it('Show the result of a + b negative numbers', () => {
     assert.equal(calculateNumber(-7, -3), -10);
   });
-  it('Show the result of a + b numbers, when the b is decimal', function() {
+  it('Show the result of a + b numbers, when the b is decimal', () => {
     assert.equal(calculateNumber(5, 0.0), 5);
   });
-  it('Without arguments', function() {
+  it('Without arguments', () => {
     //assert(isNaN(calculateNumber(0),), 'no have a number');
     //assert(isNaN(calculateNumber(0,)));
     //assert(isNaN(calculateNumber()));
   });
-  it('Processing with number zero', function() {
+  it('Processing with number zero', () => {
     assert.equal(calculateNumber(0, 0), 0);
     assert.equal(calculateNumber(0.0, 0), 0);
   });
-  it('Tetsting for two integers numbers', function() {
+  it('Tetsting for two integers numbers', () => {
     assert.equal(calculateNumber(0, 0), 0);
     assert.equal(calculateNumber(0.0, 0), 0);
   });
-  it('Testing throw error', function () {
+  it('Testing throw error', () => {
     assert.throws(() => calculateNumber(NaN, 5), 'The argument not is a number');
   });
 });
